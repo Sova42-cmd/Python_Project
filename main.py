22d60c6e6dc90c35a97fc19644409e8c059180f0
Sova42-cmd <steamsova42@gmail.com>
2025-09-17 13:31:21 +0400

    variable types

--- main.py
+++ main.py
@@ -1,4 +1,80 @@
-print("Hiii :3")
-print("How are you? :3")
-print("Does this really work?")
-print("Why does the name of repository doesn't change")
+# print("Hiii :3")
+# print("How are you? :3")
+# print("Does this really work?")
+# print("Why does the name of repository doesn't change?")
+
+# Variables – A container for value (string, integer, float, boolean)
+
+# String Formatting – F-string, Placeholders and Modifiers
+
+# txt = f"The temperature is 23Cº"
+# print(txt)
+
+# length = 50
+# txtLenght = f"The lenght of the boat is {length}m."
+# print(txtLenght)
+#
+# book = "Dante's Divine Comedy"
+# txtBook = f"{book}, is considered one of the classics."
+# print(txtBook)
+#
+# album = "Ptah, The El Daoud"
+# txtAlbum = f"{album}, is a great spiritual Jazz album"
+# print(txtAlbum)
+
+# Integers
+
+# age = 89
+# print(f"Buddy Guy is {age} years old.")
+#
+# quantity = 7
+# print(f"There are {quantity} wonders of the world.")
+# print(f"There are also {quantity} days in a week.")
+# print(f"As well as {quantity} colors in the rainbow!")
+
+# Floats
+# a number with a decimal – 10.99, 9.99 etc.
+
+# price = 19.99
+# print(f"Buy our special coffee maker for only {price}$")
+#
+# gpa = 3.1
+# print(f"In a random class student's average gpa is: {gpa}")
+#
+# distance = 1.2785
+# print(f"The distance between Earth and Saturn is about {distance} Billion km.")
+
+# Boolean
+# data type that only be true or false.
+
+# is_student = True
+#
+# print(f"Are you a student?: {is_student}")
+
+# has_graduated = True
+#
+# if has_graduated:
+#     print("Congratulations!")
+# else:
+#     print("Sorry, not yet.")
+
+# is_discounted = True
+#
+# if is_discounted:
+#     print("Discount – 15%")
+# else:
+#     print("Undiscounted")
+
+# is_online = True
+#
+# if is_online:
+#     print("Online")
+# else:
+#     print("Last seen recently")
+
+
+
+
+
+
+
