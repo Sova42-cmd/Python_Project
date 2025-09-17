bc75276a6fa97dcfdd06a973e16cf8b16d7203cb
Sova42-cmd <steamsova42@gmail.com>
2025-09-17 14:52:22 +0400

    typecasting, input(), + exercises.

--- main.py
+++ main.py
@@ -1,80 +1,67 @@
-# print("Hiii :3")
-# print("How are you? :3")
-# print("Does this really work?")
-# print("Why does the name of repository doesn't change?")
+# Typecasting = a process of converting a variable from one data type to another!
+#str(), int(), float(), bool()
+#especially useful for handling user input, since it's mainly str().
 
-# Variables – A container for value (string, integer, float, boolean)
-
-# String Formatting – F-string, Placeholders and Modifiers
-
-# txt = f"The temperature is 23Cº"
-# print(txt)
-
-# length = 50
-# txtLenght = f"The lenght of the boat is {length}m."
-# print(txtLenght)
-#
-# book = "Dante's Divine Comedy"
-# txtBook = f"{book}, is considered one of the classics."
-# print(txtBook)
-#
-# album = "Ptah, The El Daoud"
-# txtAlbum = f"{album}, is a great spiritual Jazz album"
-# print(txtAlbum)
-
-# Integers
-
-# age = 89
-# print(f"Buddy Guy is {age} years old.")
-#
-# quantity = 7
-# print(f"There are {quantity} wonders of the world.")
-# print(f"There are also {quantity} days in a week.")
-# print(f"As well as {quantity} colors in the rainbow!")
-
-# Floats
-# a number with a decimal – 10.99, 9.99 etc.
-
-# price = 19.99
-# print(f"Buy our special coffee maker for only {price}$")
-#
-# gpa = 3.1
-# print(f"In a random class student's average gpa is: {gpa}")
-#
-# distance = 1.2785
-# print(f"The distance between Earth and Saturn is about {distance} Billion km.")
-
-# Boolean
-# data type that only be true or false.
-
-# is_student = True
-#
-# print(f"Are you a student?: {is_student}")
-
-# has_graduated = True
-#
-# if has_graduated:
-#     print("Congratulations!")
-# else:
-#     print("Sorry, not yet.")
-
-# is_discounted = True
-#
-# if is_discounted:
-#     print("Discount – 15%")
-# else:
-#     print("Undiscounted")
-
+# name = "Sova42"
+# experience = 17
+# version = 42.24
 # is_online = True
+
+# print(type(is_online))
+# print(type(version))
+
+# version = int(version)
+# print(version)
+
+# experience = float(experience)
+# print(experience)
+
+# Could be used for entering a name/login/password
+# name = bool(name)
+# print(name)
+
+# input () = A function that prompts the user to enter a data.
+#            Returns the entered data as a string.
+
+# name = input("What is your name?")
+# print(f"Hello {name}!! :3")
 #
-# if is_online:
-#     print("Online")
-# else:
-#     print("Last seen recently")
+# float = input("What is your exact GPA?")
+# print(f"You dumbass nigga!")
+#
+# age = input("Anyway, how old are you?")
+# #Use typecasting, since can't add int to str.
+# #Or enclose an input with a typecast -> int(input("")) or bool(input("")) !
+# age = int(age)
+# age = age + 1
+# print(f"You're going to be {age} years old next year!.")
 
+#Exercise #1 – Rectangle area calculation
 
+# lenght = float(input("What's the lenght of the rectangle?"))
+# width = float(input("What's the width of the rectangle?"))
+# area = lenght * width
+#
+# print(f"The area of the rectangle is → {area}")
 
+#Exercise #2 - Shopping cart program
 
+# item = input("What item would you like to purchase?")
+# price = float(input("What's the price of your item?"))
+# quantity = int(input("How many would you like to purchase?"))
+#
+# total = price * quantity
+# print(f"Your total price is ${total}")
 
+# Madlibs game – word game where create a story by filling in blanks with random words
 
-
+# noun1 = input("Enter the first noun (thing): ")
+# adjective1 = input("Enter the first adjective (description): ")
+# noun2 = input("Enter the second noun (thing): ")
+# adjective2 = input("Enter the second adjective (description): ")
+# verb1 = input("Enter the a verb (action): ")
+#
+# print(f"I woke up in a strange place, probably in {noun1}.")
+# print(f"After some time I heard a weird noise, seemed rahter {adjective1}.")
+# print(f"It was {noun2} HIMSELF! pretty {adjective2}.")
+# print(f"Consfused I started {verb1}, and woke up the second time. It was just a dream.")
