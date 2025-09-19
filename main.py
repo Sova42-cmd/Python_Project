81717e20c01a40307c3aa6a998d44683885a3898
Sova42-cmd <steamsova42@gmail.com>
2025-09-19 13:28:02 +0400

    Simple caculator

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
-#what the fuck github, stop tweaking
-#okay he stopped tweaking, but 10th push is too much for a single day.
+# Python very basic calculator
+# my version
 
-#revision
-
-# lenght = float(input("Enter the length of the rectangle: "))
-# width = float(input("Enter the width of the rectangle: "))
+# firstNumber = float(input("Enter first number: "))
+# secondNumber = float(input("Enter second number: "))
 #
-# perimeter = 2 * (lenght + width)
-# area = lenght * width
-# print(f"The perimeter of the rectangle is {perimeter} and the area of the rectangle is {area}")
-
-# gpa = float(input("Declare, if you dare, the digits that measure your scholarly worth (0-5): "))
+# operetor = str(input("What should we do dawg? → +,-,*,/: "))
 #
-# if gpa > 5:
-#     print("The council laughs at thy deceit!")
-# elif gpa > 3.5:
-#     print("Further trials yet await...")
-# elif gpa < 3.5:
-#     print("A tragic academic fate awaits you mortal!")
+# if operetor == "+":
+#     print(firstNumber + secondNumber)
+# elif operetor == "-":
+#     print(firstNumber - secondNumber)
+# elif operetor == "*":
+#     print(firstNumber * secondNumber)
+# elif operetor == "/":
+#     print(firstNumber / secondNumber)
 
-# Pay attention to the order of checks future Sova42!
+# yt version
 
-import random
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
-response = input("Would you like to sign my petition? (y/n): ")
-if response == "y":
-    print("Thanks!")
-elif response == "n":
-    reposnes = [
-        "You're gonna sign this, or it will be your surviving family members",
-        "You gotta be fucking kidding me",
-        "You're gonna sign this petition or I'll follow you home and kill your dog"
-    ]
-    print(random.choice(reposnes))
-else: print("fuck you, retarded piece of shit! type in fucking either letter n or y")
+operator = str(input("Enter an operator (+ - * /) → "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 2))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 2))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 2))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 2))
+else:
+    print(f"Something went wrong, I can't {operator} your numbers.")
+
+# yt's code is more detailed, I'll consider that in the future.
+
+
+
