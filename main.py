c52a804e3809c3ef9b320d1063fc4b9b56719df4
Sova42-cmd <steamsova42@gmail.com>
2025-09-20 14:10:29 +0400

    compound interest calc(worth improving in the future, add confirmation stage.)

--- main.py
+++ main.py
@@ -1,68 +1,32 @@
-# format specifiers = {value: flags} format a value based on what flags are insterted
+# Compound interest calculator
 
-# price1 = 3000000.14159
-# price2 = -420.69
-# price3 = 99.19
+initBalance = 0
+time = 0
+rate = 0
 
-# print(f"Price 1 is {price1: .2f}")
-# print(f"Price 2 is {price2: .3f}")
-# print(f"Price 3 is {price3: .1f}")
-# same as {round(price, 2/3/1)}
-
-# print(f"Price 1 is {price1:<10}")
-# print(f"Price 2 is {price2:>100}")
-# print(f"Price 3 is {price3:^50}")
-# space lenght before the numbers.
-# < justifies the spaces to the left.
-# > justfies the spaces to the right.
-# ^ justifies the spaces to the center.
-
-# print(f"Price 1 is {price1:+,.2f}")
-# print(f"Price 2 is {price2:+,}")
-# print(f"Price 3 is {price3:+,}")
-# , separates each thousand with , +3,000,000.14
-
-# While loops = execute some code WHILE some condition remains true!
-
-# name = input("What's your name?: ")
-#
-# while name == "":
-#     print("You didn't enter a name")
-#     name = input("What's your name?: ")
-# else:
-#     print(f"Hi {name}")
-
-# promt = input("A̴̡̝̜̬̲̱̙͌́̈́̓͗n̷̯̱̠͚͕͆̀̀̌̔͜͠ ̷̧̣̤͙͈̙̥̙͕̠͓̟̼͙̩͂͐̚͝ẻ̵̙͍̯̗̤͗ỹ̴̢͎͚͖̱̗̟͙͈̳̞́̒́͋̐́̇̊̓͠ę̷͔̮̥̝̏̏́̋̓͂́̃̇̇̍l̶̢̡̛͕͙̲͚̲̣̯̞ĕ̸̢̞͔̞͙͚̱͖̰͉͍̟̩͔̈̈́͘͘͜͝͝s̴̜̘̟̣̮̰͚̯͌̽͛̒̽̀͐͗͗́͌͘͝s̵̹̗̝̞̬̮̞͖̼͋͂͛́̑̋ ̷͉͕̩̹̯͉̓́́͛̓́̑̾a̵̢̛͉̰̰̘̩̓̓̀͂b̴̘̜͖͔̄͒̉͌͑̊̽̾̉̂͛͘õ̸̢̺̳͙̘̗̔̕m̴̲̟͎͋̆͒̒̆̂̓̿̏̋̆͊̄̅i̶͕̦͙͔̱͓̤̠͎̇̄̂̚͝ǹ̸̨̡̬̣͕̪̭͇͈̪̟̮̮̩̄a̵͍̹̅̾͒̊͋̔t̷̝̩̘͉̰̺̮͙̘̃͂̉̀͛̑̀̿̎̈̔̄͝ï̴̢̢̺͙̔̈́̊̽̇͆̀̐̌̽̽̚͝ơ̶̧̛̩̬̭͎̤̝̲̲̪͓̩̰̋̇͛̔̀͌͗͘͘n̸̛̘̒̃̈́͑̋̑̀͆̔̒͛̀̅͘ ̸̧͍̗͈͚͕̰̭̟̮̝̪̇̃̋͒̐͛͑̅̚͠w̵̦̐̈́̽̅͑̌̾̍̈́̉̕͘̚i̷̜͋̍̓̃̾̓t̸̢̢̢̺̥͓͚̟̳̪͎̰̹̬̿̽͐͐͒̉̅̔̑͜ḩ̸̳̩͕̩̹̤̣̭͖̮͍̏͋̅͋̓̑̎̈́̈́̈́̌̀̚͘̚ ̶̧̧̼͕̹̠̞̻̼̅̈́̉s̶̨̛̞̞̦̦͖̲̩͔͙̟̣̫̘͎̆̅̈́̒͂͛̇̑͒̅̕͘͝͠ḙ̵̡̟̦͔̮̬̩̲̭̜͂ͅv̸͙͖́̈͆̾͜ͅȩ̸͎͓͓͍̐̊͐̃̈́͑́̇̏̾̕n̸̛̠̲̱̯̰͑̏͊͊͑̃͛̚ ̴̢̜͕͖̱̜̗̩̜̓̒̆m̵̧̲̦͓̺̜̗͚̜̞͇̠̈́̕͘͠o̶̧̨̢͈̯͚͈̠̝͕̤͙͗͌̚͜͝ͅȕ̴̲͓͎̕ṯ̴̨̞͇͖͉̈́̾̈́̾͊̉̏̅͛͗̈́͝ĥ̵̨̛̩͇͇̬̘̗̱̮͙̼̄͂̈́̈́̋̌̈͑̈ͅš̶̴̢̼̼̬̘͓͕̭̜͕͖̱̜̗̩̜͗̽̓̒̆̚͝m̵̧̲̦͓̺̜̗͚̜̞͇̠̈́̕͘͠o̶̧̨̢͈̯͚͈̠̝͕̤͙͗͌̚͜͝ͅȕ̴̲͓͎̕ṯ̴̨̞͇͖͉̈́̾̈́̾͊̉̏̅͛͗̈́͝ĥ̵̨̛̩͇͇̬̘̗̱̮͙̼̄͂̈́̈́̋̌̈͑̈ͅš̶̴̢̼̼̬̘͓͕̭̜͕͖̱̜̗̩̜͗̽̓̒̆̚͝m̵̧̲̦͓̺̜̗͚̜̞͇̠̈́̕͘͠o̶̧̨̢͈̯͚͈̠̝͕̤͙͗͌̚͜͝ͅȕ̴̲͓͎̕ṯ̴̨̞͇͖͉̈́̾̈́̾͊̉̏̅͛͗̈́͝ĥ̵̨̛̩͇͇̬̘̗̱̮͙̼̄͂̈́̈́̋̌̈͑̈ͅš̶̼̼̬̘͓͕̭͗̽̚͝.̸̀̈͘???͝: ̢")
-#
-# while promt == "what?":
-#     print("LET ME OUT")
-# Hahha scary kinda
-
-# age = int(input("How old are you?: "))
-#
-# while age < 0:
-#     print("Something went wrong, try again")
-#     age = int(input("How old are you?: "))
-# else:
-#     print(f"You're {age} years old")
-
-# food = input("What food do you like? (enter q to exit): ")
-#
-# while not food == "q":
-#     print(f"I like {food} too!")
-#     food = input("What else do you like? (enter q to exit): ")
-# else:
-#     print("Goodbye!")
-
-num = int(input("Guess a number from 1 to 10: "))
-
-while num < 1 or num > 10:
-    print("Wrong!")
-    num = int(input("Enter a number from 1 to 10: "))
+while initBalance <= 0:
+    initBalance = float(input("Enter your initial balance (in $): "))
+    if initBalance <= 0:
+        print("can't be negative or zero")
+    # elif initBalance > 0:
+        # print(f"Your initial balance is {initBalance}, confirm? (y/n): ")
+    # elif initialBalance == "y":
+    #     print(f"Your inital balance is {initBalance}")
+    # elif initBalance == "n":
+    #     initBalance = float((input("Enter your initial balance (in $): ")))
 else:
-    print(f"Is your number... {num}? Hehe")
+    print(f"Your initial balance is {initBalance}")
 
+while rate <= 0:
+    rate = float(input("Enter rate of change in % (e.g. 30%): "))
+    if rate <= 0:
+        print("can't be negative or zero")
+else:
+    print(f"Your rate of change is {rate}")
 
-
-
-
+while time <= 0:
+    time = float(input("Enter number of time periods (in years): "))
+    if time <= 0:
+        print("can't be negative or zero")
+else:
+    print(f"in {time} years you'll have this much green ${round(initBalance * (pow((rate/100)+1, time)), 3)}")
