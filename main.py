94cf6cab9590c34fc061eb349a9a53f8a2fc4187
Sova42-cmd <steamsova42@gmail.com>
2025-09-22 12:45:16 +0400

    2D collections + num_pad

--- main.py
+++ main.py
@@ -1,86 +1,42 @@
-# nested loops = a loop within another loop (outer, inner)
-#                outer loop:
-#                   inner loop:
-# for y in range(1,4):
-#     for x in range(1, 10):
-#         print(x, end=" ")
+# 2D collections
+
+# movies = ["The Matrix", "Interstellar", "Blade Runner 2049"]
+# directors = ["Wachowskis", "Christopher Nolan", "Denis Villeneuve"]
+# years = [1999, 2014, 2017]
+
+# cinema = [movies, directors, years]
+# print(cinema)
+# in order to access a specific element use
+# print(cinema[0][2]) # returns Blade Runner 2049, like coordinats
+
+# you also could do
+# cinema = [["The Matrix", "Interstellar", "Blade Runner 2049"],
+#           ["Wachowskis", "Christopher Nolan", "Denis Villeneuve"],
+#           [1999, 2014, 2017]]
+# print(cinema)
+
+# for collection in cinema:
+#     for stuff in collection:
+#         print(stuff, end=" ")
 #     print()
 
-# rows = int(input("Enter the amount of rows: "))
-# columns = int(input("Enter the amount of colums: "))
-# symbol = input("Enter the symbol: ")
+# movies = ["The Matrix", "Interstellar", "Blade Runner 2049"]
+# directors = ["Wachowskis", "Christopher Nolan", "Denis Villeneuve"]
+# years = [1999, 2014, 2017]
 #
-# for y in range (rows):
-#     for x in range(columns):
-#         print(symbol, end="")
+# movie_info = list(zip(movies, directors,years)) #zip() brings together each [0][1]... element of each set,
+# print(movie_info) #movie_info[1] → all info about Interstellar etc.
+
+# num_pad = ((1, 2, 3),
+#            (4, 5, 6),
+#            (7, 8, 9),
+#            ("*", 0, "#"))
+#
+# for row in num_pad:
+#     for num in row:
+#         print(num, end=" ")
 #     print()
 
-# collections = single "variable" used to store multiple values
-    #   List = [] ordered and changable. Duplicates OK
-    #   Set = {} unordered and immutable, but Add/Remove OK. NO duplicates.
-    #   Tuple = () ordered and unchagnable. Dubplicates OK. FASTER
-    #   Dictionary = ordered, mutable – for later.
 
-# List
-# albums = ["Bridges", "TWM", "Cryptic Writings", "Head Hunters"]
 
-# print(albums[0]) #first element
-# print(albums[3]) #4th element
-# print(albums[:2]) #all elements until the 3rd one
-
-# print("TWM" in albums) #returns True
-# print("asd" in albums) #returns False
-
-# albums.append("Octavarium") #adds an element to the end.
-# albums.remove("TWM") #removes selected element.
-# albums.insert(1, "Hot Rats") #insert element to prefered place.
-# albums.sort() #sorts elements alphabetically.
-# albums.reverse() #reverses current order of element.
-# albums.clear() #removes all the elements.
-# print(albums.count("TWM")) #counts how many of the same elements are there.
-
-# print(albums)
-
-# Set
-# languages = {"Python", "JS", "Java", "C"}
-
-# print(languages[0]) # → can't do this, because sets are unordered
-# languages.remove("JS") # → acceptable
-# languages.add("php") # → acceptable
-# languages.pop() # → deleted first element (randomly)
-#
-# print(languages)
-
-# Tuple
-# planets = ("Mercury", "Venus", "Earth", "Mars", "Saturn")
-
-#only two methods we have access to.
-
-# print(planets.index("Venus")) #Venus's index is 1.
-# print(planets.count("Earth")) #Only 1 Earth.
-
-# Shopping cart program
-
-foods = []
-prices = []
-total = 0
-
-while True:
-    food = input("Enter a food to purchase (enter q to exit): ")
-    if food.lower() == "q":
-        break
-    else:
-        price = float(input(f"Enter the price of {food}: "))
-        foods.append(food)
-        prices.append(price)
-
-print("–––Your Cart–––")
-
-for food in foods:
-    print(food, end=", ")
-
-for price in prices:
-    total += price
-
-print(f" – Your total is ${total}")
 
